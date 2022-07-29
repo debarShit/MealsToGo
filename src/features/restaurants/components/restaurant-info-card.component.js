c52c7451a4e29036b2b15aa8b7ddce8a94ad519a
debarshi <dune02deb@gmail.com>
2022-07-29 14:36:05 +0530

    hooking up favourites

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { SvgXml } from "react-native-svg";
+import { View } from "react-native";
 
+import { Favourite } from "../../../components/favourites/favourite.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { Text } from "../../../components/typography/text.component";
 import star from "../../../../assets/star";
@@ -35,12 +37,15 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
 
   return (
     <RestaurantCard elevation={5}>
-      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <View>
+        <Favourite restaurant={restaurant} />
+        <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      </View>
       <Info>
         <Text variant="label">{name}</Text>
         <Section>
           <Rating>
-          {ratingArray.map((_, i) => (
+            {ratingArray.map((_, i) => (
               <SvgXml
                 key={`star-${placeId}-${i}`}
                 xml={star}
